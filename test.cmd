e8b4cad8fac350e5c1ed44c052cc7cd6da750734
Loïc PÉRON <loic.peron@gmail.com>
2022-08-30 23:39:33 +0200

    chore: add install/uninstall test scripts

---
+++ test.cmd
@@ -0,0 +1,1 @@
+powershell.exe -file "test.ps1"
---
+++ test.ps1
@@ -0,0 +1,6 @@
+iex "& {$(irm get.scoop.sh)} -RunAsAdmin"
+Get-ChildItem ".\bucket" -Filter *.json | Sort |
+Foreach-Object {
+    scoop install $_.FullName
+    scoop uninstall $_.Basename
+ }

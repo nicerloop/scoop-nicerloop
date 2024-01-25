ed2727dc5c91cd678e0636ff1894f7a47b8a45ad
Loïc PÉRON <loic.peron@gmail.com>
2024-01-25 11:08:22 +0100

    bucket: use master branch

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "nicerloop/scoop-nicerloop:main"
+    [String]$upstream = "nicerloop/scoop-nicerloop:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
